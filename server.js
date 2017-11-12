73147fd45c75c0aeb00fcdd5980ba3bbe0d6402f
Milan Urosevic <kobimilan995@yahoo.com>
2017-11-12 12:21:14 +0100

    maintenance

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	fs.appendFile('server.log', log + '\n');
+	next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
